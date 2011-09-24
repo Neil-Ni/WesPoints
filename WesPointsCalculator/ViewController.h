c7f1bc284ff29aa3a54057dfb35487dec9a2fd15
Diego Calderon <dcalderon@Diegos-MacBook-Pro.local>
2011-09-24 15:19:51 -0400

    Initial Commit

---
+++ WesPointsCalculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WesPointsCalculator
+//
+//  Created by Diego Calderon on 9/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
