af56dc9037f44b651477129a041978ac9706a897
Tzu-Yang Ni <NNi@Tzu-Yang-Nis-MacBook-Pro.local>
2011-10-06 13:49:46 -0400

    I added the files of this project.

---
+++ point calculator/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  point calculator
+//
+//  Created by Tzu-Yang Ni on 9/23/11.
+//  Copyright (c) 2011 yoseka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
