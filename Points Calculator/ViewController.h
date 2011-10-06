c8bd76f5da25deedf160d3301c947e9d5b2a51e6
Lisa <lsy@wesleyan.edu>
2011-10-06 13:55:42 -0400

    Initial changes, lisa.

--- Points Calculator/ViewController.h
+++ Points Calculator/ViewController.h
@@ -8,6 +8,31 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITextFieldDelegate>{
+    UITextField *PointsTextFields;
+    UITextField *MealsTextFields;
+    UILabel *CurrentDate;
+    UILabel *DaysLeft;
+    UILabel *SpendingRatePerDay;
+    UILabel *IdealSpendingRatePerDay;
+    UILabel *WeeksLeft;
+    UILabel *SpendingRatePerWeek;
+    NSString *Days;
+    NSString *Weeks;
+    NSInteger dayspast;
+    NSInteger daysmore;
+    float Points;
+    NSInteger Meals;
+}
+
+@property (strong, nonatomic) IBOutlet UITextField *PointsTextFields;
+@property (strong, nonatomic) IBOutlet UITextField *MealsTextFields;
+@property (strong, nonatomic) IBOutlet UILabel *CurrentDate;
+@property (strong, nonatomic) IBOutlet UILabel *DaysLeft;
+@property (strong, nonatomic) IBOutlet UILabel *SpendingRatePerDay;
+@property (strong, nonatomic) IBOutlet UILabel *IdealSpendingRatePerDay;
+@property (strong, nonatomic) IBOutlet UILabel *WeeksLeft;
+@property (strong, nonatomic) IBOutlet UILabel *SpendingRatePerWeek;
+@property (strong, nonatomic) IBOutlet UILabel *IdealSpendingRatePerWeek;
 
 @end
--- Points Calculator/ViewController.m
+++ Points Calculator/ViewController.m
@@ -9,6 +9,48 @@
 #import "ViewController.h"
 
 @implementation ViewController
+@synthesize PointsTextFields= _PointsTextField;
+@synthesize MealsTextFields= _MealsTextFields;
+@synthesize CurrentDate= _CurrentDate;
+@synthesize DaysLeft=_DaysLeft;
+@synthesize SpendingRatePerDay=_SpendingRatePerDay;
+@synthesize IdealSpendingRatePerDay=_IdealSpendingRatePerDay;
+@synthesize WeeksLeft=_WeeksLeft;
+@synthesize SpendingRatePerWeek=_SpendingRatePerWeek;
+@synthesize IdealSpendingRatePerWeek=_IdealSpendingRatePerWeek;
+
+- (BOOL)textFieldShouldReturn:(UITextField *)theTextField {
+    
+    //POINTS TEXT FIELD
+    if (theTextField == self.PointsTextFields) {
+        [theTextField resignFirstResponder];
+        double mydouble = [self.PointsTextFields.text doubleValue];
+        Points = (float)(mydouble);
+        
+        //leftoverpoints/dayspast
+        
+        self.SpendingRatePerDay.text = [NSString stringWithFormat:@"%f", Points/(float)dayspast];
+        //totalpoints/dayspast
+        self.IdealSpendingRatePerDay = [NSString stringWithFormat:@"%f", Points/(float)(dayspast+daysmore)];
+    }
+    //MEALS TEXT FIELD
+    if (theTextField == self.MealsTextFields) {
+        [theTextField resignFirstResponder];
+        double mydouble2 = [self.MealsTextFields.text doubleValue];
+        Meals = (int)(mydouble2 + (mydouble2>0 ? 0.5 : -0.5));
+        
+        //leftoverpoints/dayspast
+        
+        self.SpendingRatePerWeek.text = [NSString stringWithFormat:@"%f", Meals/(float)dayspast];
+        self.IdealSpendingRatePerWeek.text = [NSString stringWithFormat:@"%f", Meals/(float)(dayspast+daysmore)];
+    }
+    return YES;
+}
+
+
+
+
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -20,12 +62,49 @@
 
 - (void)viewDidLoad
 {
+    NSDate *now = [NSDate date];
+    NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+    [dateFormat setDateFormat:@"MM/dd/yyyy"];
+    NSString *dateString = [dateFormat stringFromDate: now];
+    NSLog(@"date: %@", dateString); 
+    //prints out on the terminal right away
+    self.CurrentDate.text = dateString;
+
+    
+    NSDate *schoolstarts = [dateFormat dateFromString:@"09/05/2011"];
+    NSDate *schoolends = [dateFormat dateFromString:@"12/18/2011"];
+    NSInteger datenow = [[NSCalendar currentCalendar] ordinalityOfUnit:(NSDayCalendarUnit) inUnit:(NSEraCalendarUnit) forDate:now];
+    NSInteger dateschoolends = [[NSCalendar currentCalendar] ordinalityOfUnit:(NSDayCalendarUnit) inUnit:(NSEraCalendarUnit) forDate:schoolends];
+    
+    self.DaysLeft.text = [NSString stringWithFormat:@"%d",dateschoolends-datenow];
+    
+    NSInteger dateschoolstarts = [[NSCalendar currentCalendar] ordinalityOfUnit:(NSDayCalendarUnit) inUnit:(NSEraCalendarUnit) forDate:schoolstarts];
+        //This converts the DATE from format [MM/dd/yyyy] to INTEGER to be counted.
+    
+    dayspast = datenow - dateschoolstarts;
+    daysmore = dateschoolends - datenow;
+    NSLog(@"dayspast: %d", dayspast);
+    NSLog(@"daysmore: %d", daysmore);
+    
+    self.WeeksLeft.text = [NSString stringWithFormat:@"%f",(dateschoolends-datenow)/7.0];
+                          
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)viewDidUnload
 {
+
+    [self setCurrentDate:nil];
+    [self setDaysLeft:nil];
+    [self setSpendingRatePerDay:nil];
+    [self setIdealSpendingRatePerDay:nil];
+    [self setWeeksLeft:nil];
+    [self setSpendingRatePerWeek:nil];
+    [self setIdealSpendingRatePerWeek:nil];
+    [self setPointsTextFields:nil];
+    [self setMealsTextFields:nil];
+    
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
